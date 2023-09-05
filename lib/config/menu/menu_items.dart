2278acde54e79d17c17295ef8ad1f50a9b52917a
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-09-04 21:38:04 -0500

    feat(widgets_app): Add basic routes to the app

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -24,7 +24,7 @@ const appMenuItems = <MenuItem>[
   MenuItem(
     title: 'Cards',
     subtitle: 'Card containers styled with Flutter',
-    link: '/card',
+    link: '/cards',
     icon: Icons.credit_card_outlined,
   ),
 ];
