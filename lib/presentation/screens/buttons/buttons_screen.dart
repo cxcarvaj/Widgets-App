544f7caedbe2dc9cfcb25e97b6fb87751a059352
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-09-04 21:55:28 -0500

    feat(widgets_app): Improvements

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ButtonScreen extends StatelessWidget {
+  static const String routeName = 'button_screen';
+
   const ButtonScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
+  static const String routeName = 'card_screen';
   const CardScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
