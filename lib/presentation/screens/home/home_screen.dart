ba07875131b1a396cf7112793f9010a65cc1152d
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-09-04 21:22:36 -0500

    feat(widgets_app): Add the home screen implementation

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,43 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Widgets on Flutter with Material 3'),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final item = appMenuItems[index];
+        return _CustomListTile(item: item);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.item,
+  });
+
+  final MenuItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      title: Text(item.title),
+      subtitle: Text(item.subtitle),
+      leading: Icon(item.icon, color: colors.primary),
+      trailing: const Icon(Icons.chevron_right),
+      onTap: () => Navigator.pushNamed(context, item.link),
     );
   }
 }
