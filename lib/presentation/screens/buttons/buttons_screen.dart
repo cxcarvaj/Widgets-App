c99d6831dea6143440d8fafdc12879582f14bccb
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-09-05 19:44:37 -0500

    feat(widgets_app): Finish the buttons screen

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -77,13 +77,11 @@ class _ButtonsView extends StatelessWidget {
               icon: const Icon(Icons.account_box_outlined),
               label: const Text('Text Icon'),
             ),
-            // TODO: custom button
-
+            const CustomButton(),
             IconButton(
               onPressed: () {},
               icon: const Icon(Icons.app_registration_rounded),
             ),
-
             IconButton(
               onPressed: () {},
               icon: const Icon(Icons.app_registration_rounded),
@@ -93,6 +91,32 @@ class _ButtonsView extends StatelessWidget {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Custom Button',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
         ),
       ),
     );
