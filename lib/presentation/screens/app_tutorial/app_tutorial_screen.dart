972b0225f52b5bbc6d8dbba7d9d6a0af7a8c915e
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-09-05 20:22:02 -0500

    feat(widgets_app): Create the missing screens of the app

---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTutorialScreen extends StatelessWidget {
+  static const String routeName = 'tutorial_screen';
+
+  const AppTutorialScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UIControlScreen extends StatelessWidget {
+  static const String routeName = 'ui_control_screen';
+
+  const UIControlScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
